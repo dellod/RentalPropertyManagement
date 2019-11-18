482cd00a1f89ec4cd45e57880bfb842dd8584486
Daryl <43867408+dellod@users.noreply.github.com>
2019-11-18 13:51:12 -0700

    Added more ManagerGUI.

--- View/ManagerGUI.java
+++ View/ManagerGUI.java
@@ -3,10 +3,18 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class ManagerGUI {
 
 	private JFrame frmRentalPropertymanager;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -37,8 +45,47 @@ public class ManagerGUI {
 	private void initialize() {
 		frmRentalPropertymanager = new JFrame();
 		frmRentalPropertymanager.setTitle("Rental Property (Manager)");
-		frmRentalPropertymanager.setBounds(100, 100, 450, 300);
+		frmRentalPropertymanager.setBounds(100, 100, 650, 450);
 		frmRentalPropertymanager.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frmRentalPropertymanager.getContentPane().setLayout(null);
+		
+		JLabel lblWelcomeManager = new JLabel("Welcome Manager!");
+		lblWelcomeManager.setFont(new Font("Segoe UI Black", Font.BOLD | Font.ITALIC, 20));
+		lblWelcomeManager.setBounds(209, 69, 222, 39);
+		frmRentalPropertymanager.getContentPane().add(lblWelcomeManager);
+		
+		JLabel lblPleaseEnterUsername = new JLabel("Please enter username and password (Don't have account? Register below!)");
+		lblPleaseEnterUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPleaseEnterUsername.setBounds(116, 142, 423, 13);
+		frmRentalPropertymanager.getContentPane().add(lblPleaseEnterUsername);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblUsername.setBounds(209, 165, 66, 13);
+		frmRentalPropertymanager.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPassword.setBounds(209, 200, 66, 13);
+		frmRentalPropertymanager.getContentPane().add(lblPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(285, 163, 96, 19);
+		frmRentalPropertymanager.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(285, 198, 96, 19);
+		frmRentalPropertymanager.getContentPane().add(passwordField);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnLogin.setBounds(209, 243, 85, 21);
+		frmRentalPropertymanager.getContentPane().add(btnLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnRegister.setBounds(314, 243, 85, 21);
+		frmRentalPropertymanager.getContentPane().add(btnRegister);
 	}
-
 }
